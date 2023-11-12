b941b20531384f8fd841aa5d092e5f29535170f3
MarcVinicius <viniciusmarcos.01@hotmail.com>
2023-11-11 22:43:52 -0300

    Alteração na procedure e criação de tela

--- Procedure-2.sql
+++ Procedure-2.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE PROCEDURE AA_MVSIS_ADDREMOVECLIENT
+CREATE OR REPLACE PROCEDURE AA_MVSIS_ADDREMOVECLIENTD
             (pACAO IN VARCHAR,
              pCODUSUR IN PCUSUARI.CODUSUR%TYPE,
              pCODCLI IN PCCLIENT.CODCLI%TYPE,
              pCODUSURSUB IN PCUSUARI.CODUSUR%TYPE,
              pCAMPOS IN VARCHAR,
-             pUTILIZAPAR IN VARCHAR(1), --ACEITA S OU M
-             pTIPOCOD IN VARCHAR(3) DEFAULT = 'COD')
+             pUTILIZAPAR IN VARCHAR, --ACEITA S OU M
+             pTIPOCOD IN VARCHAR DEFAULT 'COD')
 IS
     vQTCLI NUMBER;
     vQTCLIPAR NUMBER;
@@ -20,6 +20,7 @@ IS
     vQTCLIPCCLIENT2 NUMBER;
     vQTCLIPCCLIENT3 NUMBER;
     vCONTADOR_ NUMBER := 0;
+    vTEM_ NUMBER := 0;
     vCAMPOOBS PCUSUARI.OBSFORCAVENDAS4%TYPE;
 BEGIN
     --DEFININDO MENSAGEM INICIAL
@@ -28,18 +29,20 @@ BEGIN
     --DEFININDO CODIGO CLIENTE
     IF pTIPOCOD = 'COD' THEN
         vCODCLI := pCODCLI;
-    ELSE:
-        SELECT CODCLI INTO vCODCLI 
-        FROM PCCLIENT 
-        WHERE TRIM(REPLACE(TRANSLATE(CGCENT, '.-/', ' '), ' ', '')) = TRIM(REPLACE(TRANSLATE(pCODCLI, '.-/', ' '), ' ', ''));
+    ELSE
+        BEGIN
+            SELECT CODCLI INTO vCODCLI 
+            FROM PCCLIENT 
+            WHERE TRIM(REPLACE(TRANSLATE(CGCENT, '.-/', ' '), ' ', '')) = TRIM(REPLACE(TRANSLATE(pCODCLI, '.-/', ' '), ' ', ''));
 
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            IF vMESS = 'SERRO' THEN
-                vMESS := 'Cliente não encontrado na base de dados';
-            ELSE:
-                vMESS := vMESS || CHR(10) || 'Cliente não encontrado na base de dados';
-            END IF;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                IF vMESS = 'SERRO' THEN
+                    vMESS := 'Cliente não encontrado na base de dados';
+                ELSE
+                    vMESS := vMESS || CHR(10) || 'Cliente não encontrado na base de dados';
+                END IF;
+        END;
 
     END IF;
 
@@ -54,6 +57,7 @@ BEGIN
         ELSE
             vMESS := vMESS || CHR(10) || 'RCA(s) não existe(m) na base de dados';
         END IF;
+    END IF;
 
     IF NVL(vCLIEXISTE, 0) = 0 THEN
         IF vMESS = 'SERRO' THEN
@@ -61,20 +65,32 @@ BEGIN
         ELSE
             vMESS := vMESS || CHR(10) || 'Cliente não existe na base de dados';
         END IF;
+    END IF;
 
     --CHECANDO PAR
-    IF pUTILIZAPAR = 'S' THEN
+    IF vMESS <> 'SERRO' THEN
+        RAISE_APPLICATION_ERROR(-20000, vMESS);
+
+    ELSIF pUTILIZAPAR = 'S' THEN
         SELECT OBSFORCAVENDAS4 INTO vCAMPOOBS
         FROM PCUSUARI WHERE CODUSUR = pCODUSUR;
 
-        FOR I IN LENGTH vCAMPOOBS LOOP
-            IF SUBSTR(vCAMPOS, I, I) = '-' THEN
-                vCONTADOR_ := vCONTADOR_ + 1
+        --CONTANDO QUANTOS - TEM NA OBS DO RCA
+        FOR I IN 1..LENGTH(vCAMPOOBS) LOOP
+            IF SUBSTR(vCAMPOOBS, I, 1) = '-' THEN
+                vCONTADOR_ := vCONTADOR_ + 1;
+            END IF;
+        END LOOP;
+
+        --VERIFICANDO SE TEM - NA OBS DO RCA
+        FOR I IN 1..LENGTH(vCAMPOOBS) LOOP
+            IF SUBSTR(vCAMPOOBS, I, 1) = '-' THEN
+                vTEM_ := 1;
             END IF;
         END LOOP;
 
         IF NVL((LENGTH(TRIM(TRANSLATE(vCAMPOOBS, '0123456789-',' ')))), 1) = 0 THEN
-            IF '-' IN vCAMPOOBS THEN
+            IF vTEM_ = 1 THEN
                 IF SUBSTR(vCAMPOOBS, 4, 4) = '-' AND
                 vCONTADOR_ = 1 THEN
                     SELECT CODUSUR INTO vCODUSURPAR
@@ -114,7 +130,7 @@ BEGIN
 
 
         ----CLIENTES PAR 2
-        SELECT COUNT(1) INTO vQTCLIPAR FROM (select codcli from pcusurcli where codusur=vCODUSURPAR2 and codcli=pCODCLI
+        SELECT COUNT(1) INTO vQTCLIPAR2 FROM (select codcli from pcusurcli where codusur=vCODUSURPAR2 and codcli=pCODCLI
                                           union select codcli from pcclient where codusur1=vCODUSURPAR2 and codcli=pCODCLI
                                           union select codcli from pcclient where codusur2=vCODUSURPAR2 and codcli=pCODCLI
                                           union select codcli from pcclient where codusur3=vCODUSURPAR2 and codcli=pCODCLI);
@@ -123,95 +139,128 @@ BEGIN
 
     --CHECANDO SE NÃO HOUVE ERROS
     IF vMESS <> 'SERRO' THEN
-        RAISE_APPLICATION_ERROR(-20000, vMESS)
+        RAISE_APPLICATION_ERROR(-20000, vMESS);
 
-    --IF pACAO = 'A' THEN
     --COMEÇANDO AÇÕES
-    ELSE
-        
+    ELSE        
+        --ADCIONAR CLIENTE
         IF pACAO = 'A' THEN
-            ELSIF vQTCLI > 0 THEN
-                vMESS := 'CLIENTE JÁ CONSTA PARA O RCA';
-            ELSE
-                vMESS := 'SC';
+            IF vQTCLI > 0 THEN
+                IF vMESS = 'SERRO' THEN
+                    vMESS := 'Ciente já cadastrado para o RCA';
+                ELSE
+                    vMESS := vMESS || CHR(10) || 'Ciente já cadastrado para o RCA';
+                END IF;
             
             END IF;
 
-            IF vMESS = 'SC' THEN
+            IF vMESS = 'SERRO' THEN
 
-              --SETANDO O RCA PAR DO RCA ORIGINAL
-              SELECT TO_NUMBER(NVL(OBSFORCAVENDAS4, 0)) INTO vCODUSURPAR FROM PCUSUARI WHERE CODUSUR=pCODUSUR;
+                SELECT COUNT(1) INTO vQTCLIPCCLIENT1 FROM PCCLIENT
+                WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR1<>pCODUSURSUB;
 
-              IF vCODUSURPAR = pCODUSUR THEN
-                 vCODUSURPAR := 0;
-              END IF;
+                SELECT COUNT(1) INTO vQTCLIPCCLIENT2 FROM PCCLIENT
+                WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR2<>pCODUSURSUB;
 
-              SELECT COUNT(1) INTO vQTCLIPCCLIENT1 FROM PCCLIENT
-              WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR1<>pCODUSURSUB;
+                SELECT COUNT(1) INTO vQTCLIPCCLIENT3 FROM PCCLIENT
+                WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR3<>pCODUSURSUB;
 
-              SELECT COUNT(1) INTO vQTCLIPCCLIENT2 FROM PCCLIENT
-              WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR2<>pCODUSURSUB;
+                --ADICIONANDO O CLIENTE PARA O RCA PRINCIPAL
+                IF vQTCLIPCCLIENT1 = 0 AND
+                    SUBSTR(pCAMPOS, 1, 1) = 'S' THEN
+                    UPDATE PCCLIENT SET CODUSUR1=pCODUSUR
+                    WHERE CODCLI=pCODCLI;
 
-              SELECT COUNT(1) INTO vQTCLIPCCLIENT3 FROM PCCLIENT
-              WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR3<>pCODUSURSUB;
-
-              --ADICIONANDO O CLIENTE PARA O RCA PRINCIPAL
-              IF vQTCLIPCCLIENT1 = 0 AND
-                 SUBSTR(pCAMPOS, 1, 1) = 'S' THEN
-                  UPDATE PCCLIENT SET CODUSUR1=pCODUSUR
-                  WHERE CODCLI=pCODCLI;
-
-              ELSIF vQTCLIPCCLIENT2 = 0 AND
+                ELSIF vQTCLIPCCLIENT2 = 0 AND
                     SUBSTR(pCAMPOS, 2, 1) = 'S' THEN
-                  UPDATE PCCLIENT SET CODUSUR2=pCODUSUR
-                  WHERE CODCLI=pCODCLI;
+                    UPDATE PCCLIENT SET CODUSUR2=pCODUSUR
+                    WHERE CODCLI=pCODCLI;
 
-              ELSIF vQTCLIPCCLIENT3 = 0 AND
+                ELSIF vQTCLIPCCLIENT3 = 0 AND
                     SUBSTR(pCAMPOS, 3, 1) = 'S' THEN
-                  UPDATE PCCLIENT SET CODUSUR3=pCODUSUR
-                  WHERE CODCLI=pCODCLI;
+                    UPDATE PCCLIENT SET CODUSUR3=pCODUSUR
+                    WHERE CODCLI=pCODCLI;
 
-              ELSE
-                  INSERT INTO PCUSURCLI(CODUSUR, CODCLI, ENVIAFV)
-                  VALUES(pCODUSUR, pCODCLI, 'S');
+                ELSE
+                    INSERT INTO PCUSURCLI(CODUSUR, CODCLI, ENVIAFV)
+                    VALUES(pCODUSUR, pCODCLI, 'S');
 
-              COMMIT;
-              END IF;
+                END IF;
+                COMMIT;
 
-              --ADICIONANDO O CLIENTE PARA O VENDEDOR PAR
-              IF vQTCLIPAR = 0 THEN
-                  SELECT COUNT(1) INTO vQTCLIPCCLIENT1 FROM PCCLIENT
-                  WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR1<>pCODUSURSUB;
+                --ADICIONANDO O CLIENTE PARA O VENDEDOR PAR
+                IF pUTILIZAPAR = 'S' THEN
+                    --PAR1
+                    IF vQTCLIPAR = 0 THEN
+                        IF NVL(vCODUSURPAR, 0) NOT IN (0, pCODUSUR, 9999) THEN
+                            --VERIFICANDO CAMPOS DO CLIENTE NA PCCLIENT, SE EXISTE RCA INATIVO PARA SEREM SUBISTITUIDOS  
+                            SELECT COUNT(1) INTO vQTCLIPCCLIENT1 FROM PCCLIENT
+                            WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR1<>pCODUSURSUB;
 
-                  SELECT COUNT(1) INTO vQTCLIPCCLIENT2 FROM PCCLIENT
-                  WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR2<>pCODUSURSUB;
+                            SELECT COUNT(1) INTO vQTCLIPCCLIENT2 FROM PCCLIENT
+                            WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR2<>pCODUSURSUB;
 
-                  SELECT COUNT(1) INTO vQTCLIPCCLIENT3 FROM PCCLIENT
-                  WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR3<>pCODUSURSUB;
+                            SELECT COUNT(1) INTO vQTCLIPCCLIENT3 FROM PCCLIENT
+                            WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR3<>pCODUSURSUB;
 
-                  IF vQTCLIPCCLIENT1 = 0 AND
-                     SUBSTR(pCAMPOS, 1, 1) = 'S' THEN
-                      UPDATE PCCLIENT SET CODUSUR1=vCODUSURPAR
-                      WHERE CODCLI=pCODCLI;
+                            IF vQTCLIPCCLIENT1 = 0 AND
+                                SUBSTR(pCAMPOS, 1, 1) = 'S' THEN
+                                UPDATE PCCLIENT SET CODUSUR1=vCODUSURPAR
+                                WHERE CODCLI=pCODCLI;
 
-                  ELSIF vQTCLIPCCLIENT2 = 0 AND
-                        SUBSTR(pCAMPOS, 2, 1) = 'S' THEN
-                      UPDATE PCCLIENT SET CODUSUR2=vCODUSURPAR
-                      WHERE CODCLI=pCODCLI;
+                            ELSIF vQTCLIPCCLIENT2 = 0 AND
+                                SUBSTR(pCAMPOS, 2, 1) = 'S' THEN
+                                UPDATE PCCLIENT SET CODUSUR2=vCODUSURPAR
+                                WHERE CODCLI=pCODCLI;
 
-                  ELSIF vQTCLIPCCLIENT3 = 0 AND
-                        SUBSTR(pCAMPOS, 3, 1) = 'S' THEN
-                      UPDATE PCCLIENT SET CODUSUR3=vCODUSURPAR
-                      WHERE CODCLI=pCODCLI;
+                            ELSIF vQTCLIPCCLIENT3 = 0 AND
+                                SUBSTR(pCAMPOS, 3, 1) = 'S' THEN
+                                UPDATE PCCLIENT SET CODUSUR3=vCODUSURPAR
+                                WHERE CODCLI=pCODCLI;
 
-                  ELSE
-                      INSERT INTO PCUSURCLI(CODUSUR, CODCLI, ENVIAFV)
-                      VALUES(pCODUSUR, pCODCLI, 'S');
+                            ELSE
+                                INSERT INTO PCUSURCLI(CODUSUR, CODCLI, ENVIAFV)
+                                VALUES(vCODUSURPAR, pCODCLI, 'S');
+                            END IF;
+                        END IF;
+                        COMMIT;
+                    END IF;
 
-                  COMMIT;
-                  END IF;
+                    IF vQTCLIPAR2 = 0 THEN
+                        IF NVL(vCODUSURPAR2, 0) NOT IN (0, pCODUSUR, vCODUSURPAR, 9999) THEN
+                            --VERIFICANDO CAMPOS DO CLIENTE NA PCCLIENT, SE EXISTE RCA INATIVO PARA SEREM SUBISTITUIDOS  
+                            SELECT COUNT(1) INTO vQTCLIPCCLIENT1 FROM PCCLIENT
+                            WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR1<>pCODUSURSUB;
 
-              END IF;
+                            SELECT COUNT(1) INTO vQTCLIPCCLIENT2 FROM PCCLIENT
+                            WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR2<>pCODUSURSUB;
+
+                            SELECT COUNT(1) INTO vQTCLIPCCLIENT3 FROM PCCLIENT
+                            WHERE CODCLI=pCODCLI AND CODUSUR1 IN (SELECT CODUSUR FROM PCUSUARI WHERE DTTERMINO IS NULL) AND CODUSUR3<>pCODUSURSUB;
+
+                            IF vQTCLIPCCLIENT1 = 0 AND
+                                SUBSTR(pCAMPOS, 1, 1) = 'S' THEN
+                                UPDATE PCCLIENT SET CODUSUR1=vCODUSURPAR2
+                                WHERE CODCLI=pCODCLI;
+
+                            ELSIF vQTCLIPCCLIENT2 = 0 AND
+                                SUBSTR(pCAMPOS, 2, 1) = 'S' THEN
+                                UPDATE PCCLIENT SET CODUSUR2=vCODUSURPAR2
+                                WHERE CODCLI=pCODCLI;
+
+                            ELSIF vQTCLIPCCLIENT3 = 0 AND
+                                SUBSTR(pCAMPOS, 3, 1) = 'S' THEN
+                                UPDATE PCCLIENT SET CODUSUR3=vCODUSURPAR2
+                                WHERE CODCLI=pCODCLI;
+
+                            ELSE
+                                INSERT INTO PCUSURCLI(CODUSUR, CODCLI, ENVIAFV)
+                                VALUES(vCODUSURPAR2, pCODCLI, 'S');
+                            END IF;
+                        END IF;
+                        COMMIT;
+                    END IF;
+                END IF;
 
             ELSE
               RAISE_APPLICATION_ERROR (-20000, vMESS);
@@ -219,4 +268,4 @@ BEGIN
             END IF;
         END IF;
     END IF;
-END
+END;
